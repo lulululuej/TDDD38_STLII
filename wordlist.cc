60fe9fac1a8029607f10345b046c3d9720fda250
EJ Chow <karenchow@EJ-MacBook-Air.local>
2023-04-26 18:08:29 +0200

    question abt lambda exp

---
+++ wordlist.cc
@@ -0,0 +1,69 @@
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <iterator>
+#include <string>
+
+using namespace std;
+int main() {
+    cout << "Enter the wordlist : ";
+    vector<string> text { istream_iterator<string>{cin}, istream_iterator<string>{} };
+    vector<string> words {text};
+
+    for_each(words.begin(), words.end(), [](string &n){ cout << n << ' '; });
+    cout << "\n";
+    sort(begin(words), end(words));
+    for_each(words.begin(), words.end(), [](string &n){ cout << n << ' '; });
+    cout << "\n";
+    words.erase(unique(begin(words), end(words)), end(words));
+    for_each(words.begin(), words.end(), [](string &n){ cout << n << ' '; });
+    cout << "\n";
+
+    // the diff of capture caluse and parameter in lambda expression 
+    std::transform(words.begin(), words.end(), ostream_iterator<string>{cout, "\n"},
+    [&text](string const& words)
+    {
+        int n = count(text.begin(), text.end(), words ) ;
+        return words + ": " + to_string(n);
+        //return team_to_string(a) + " | " + b.first;
+    }
+    );
+
+    /*
+    template<class InputIt, class OutputIt, class UnaryOperation>
+    OutputIt transform(InputIt first1, InputIt last1,
+                    OutputIt d_first, UnaryOperation unary_op)
+    {
+        while (first1 != last1)
+            *d_first++ = unary_op(*first1++);
+    
+        return d_first;
+    }
+    */
+    /*
+    template<class InputIt1, class InputIt2, class OutputIt, class BinaryOperation>
+    OutputIt transform(InputIt1 first1, InputIt1 last1,
+                    InputIt2 first2, OutputIt d_first,
+                    BinaryOperation binary_op)
+    {
+        while (first1 != last1)
+            *d_first++ = binary_op(*first1++, *first2++);
+    
+        return d_first;
+    }
+    */
+
+    /*vector<string> text { istream_iterator<string>{cin},
+                          istream_iterator<string>{} };
+    vector<string> words {text};
+    
+    sort(begin(words), end(words));
+    words.erase(unique(begin(words), end(words)), end(words));
+ 
+    transform(begin(words), end(words), ostream_iterator<string>{cout, "\n"},
+              [&text](string const& word)
+              {
+                  int n = count(begin(text), end(text), word);
+                  return word + ": " + to_string(n);
+              });*/
+}
